c4f7512e12b5ab8b970074d04a8f5eaf93811b45
xmc5281 <1028251782@qq.com>
2022-08-10 16:22:33 +0800

    再次提交

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my_firstmodules_pip_fibo",  # 这个名字要独一无二的
+    version="0.1",
+    author="xmc5281",
+    author_email="1028251782@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xmc5281/mymodule",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    py_requires=["myCode"],  # 这是你存放python代码的目录
+    python_requires='>=3.6',
+)
